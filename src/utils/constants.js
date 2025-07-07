7d60b44afd0a10cfc2ca07d2e37d57090e7db355
fahim <fahimahamedweb@gmail.com>
2025-07-07 23:04:04 +0600

    added some issues

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,4 @@
-export const BASE_URL = 'http://localhost:5000/'
-
-export const NEXT_PUBLIC_SOCKET_URL = 'http://localhost:3001'
+//export const BASE_URL = 'http://localhost:5000/'
+export const BASE_URL = "https://api.trustedgpclinic.com"
+//export const NEXT_PUBLIC_SOCKET_URL = 'http://localhost:3001'
+export const NEXT_PUBLIC_SOCKET_URL= "https://ws.trustedgpclinic.com"
